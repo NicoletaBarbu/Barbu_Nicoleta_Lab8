b641a1b11a60c822d286cbf5f8bd57fa5be7a370
Nicoleta Barbu <barbunicoleta92@yahoo.com>
2020-11-19 10:09:43 +0200

    Add project files.

---
+++ Barbu_Nicoleta_Lab8/Data/Barbu_Nicoleta_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Barbu_Nicoleta_Lab8.Models;
+
+namespace Barbu_Nicoleta_Lab8.Data
+{
+    public class Barbu_Nicoleta_Lab8Context : DbContext
+    {
+        public Barbu_Nicoleta_Lab8Context (DbContextOptions<Barbu_Nicoleta_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Barbu_Nicoleta_Lab8.Models.Book> Book { get; set; }
+    }
+}
